50fac72d8d7793ece3d50d68cb37672b97ade0cd
Glademir Rodrigues <rodriguesglademir@gmail.com>
2022-02-12 18:52:34 -0300

    SistemaAluno

---
+++ sistema.js
@@ -0,0 +1,42 @@
+let nomeDeAluno = ["joao", "maria", "ana", "paulo"]
+
+for (let nome of nomeDeAluno) {
+    console.log (`Esta pessoa se chama ${nome}`)
+    }
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+// console.log(contador)
+
+//     if(contador == 0){
+//         console.log("o número atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("o número " + contador + " é par")
+//     }else{
+//         console.log(`o número ${contador} é impar`)
+//     }
+// }
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numeroDeAlunos) {
+//     //console.log(contador)
+    
+//         if(contador == 0){
+//             console.log("o número atual é zero")
+//         }else if(contador % 2 == 0){
+//             console.log("o número " + contador + " é par")
+//         }else{
+//             console.log(`o número ${contador} é impar`)
+//         }
+//         contador++;
+//     }
